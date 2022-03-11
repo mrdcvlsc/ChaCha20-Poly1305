2d54ebd3f9d100106095f2d4fb334006da0a2670
mrdcvlsc <mordecaivelasco@gmail.com>
2022-03-11 10:36:03 +0800

    added one add test for int128

--- tests/int128_add_test.cpp
+++ tests/int128_add_test.cpp
@@ -2,8 +2,10 @@
 #include "../int-128-256.hpp"
 
 int main() {
-
-    bool Tests[5];
+    size_t TEST_CASES = 6;
+    bool Tests[TEST_CASES];
+    for(size_t i=0; i<TEST_CASES; ++i)
+        Tests[i] = false;
 
     int128 MAX(0xffffffffffffffff, 0xffffffffffffffff);
     int128 ONE(0,1);
@@ -13,7 +15,6 @@ int main() {
     int128 CORRECT_SUM(0,0);
     if(SUM!=CORRECT_SUM) {
         std::cout << "int128test_max+one : FAILED\n";
-        Tests[0] = false;
     }
     else{
         std::cout << "int128test_max+one : PASSED\n";
@@ -24,7 +25,6 @@ int main() {
     SUM = MAX + TWO;
     if(SUM!=ONE) {
         std::cout << "int128test_max+two : FAILED\n";
-        Tests[1] = false;
     }
     else{
         std::cout << "int128test_max+two : PASSED\n";
@@ -36,7 +36,6 @@ int main() {
     int128 CORRECT_MAX_MAX_SUM(18446744073709551615ul,18446744073709551614ul);
     if(SUM!=CORRECT_MAX_MAX_SUM) {
         std::cout << "int128test_max+max : FAILED\n";
-        Tests[2] = false;
     }
     else{
         std::cout << "int128test_max+max : PASSED\n";
@@ -50,7 +49,6 @@ int main() {
     SUM = MAX_MSB_M5 + TEN;
     if(SUM!=CORRECT_MAX_MSB_M5_ADD_TEN) {
         std::cout << "int128test_max_msb_m5+ten : FAILED\n";
-        Tests[3] = false;
     }
     else{
         std::cout << "int128test_max_msb_m5+ten : PASSED\n";
@@ -63,27 +61,37 @@ int main() {
     SUM = MAX_M5 + MAX;
     if(SUM!=CRRTM5) {
         std::cout << "int128test_max_msb_m5+max_m5 : FAILED\n";
-        Tests[4] = false;
     }
     else{
         std::cout << "int128test_max_msb_m5+max_m5 : PASSED\n";
         Tests[4] = true;
     }
 
+    int128 X64(0, 64);
+    int128 X64_CORRECT(0, 63);
+    SUM = X64 + MAX;
+    if(SUM!=X64_CORRECT) {
+        std::cout << "int128test_max+64 : FAILED\n";
+    }
+    else{
+        std::cout << "int128test_max+64 : PASSED\n";
+        Tests[5] = true;
+    }
+    
     // end of tests
     
     size_t failed_cnt = 0;
-    for(size_t i=0; i<5; ++i) {
+    for(size_t i=0; i<TEST_CASES; ++i) {
         if(!Tests[i])
             failed_cnt++;
     }
 
     if(!failed_cnt) {
-        std::cout << "int128 add test ALL PASSED\n";
+        std::cout << "int128 add test all PASSED\n";
         return 0;
     }
     else {
-        std::cout << "int128 add test ALL FAILED\n";
+        std::cout << "int128 add test some FAILED\n";
         return 0;
     }
 }
